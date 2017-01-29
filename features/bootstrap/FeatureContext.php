b03b294fd61a179630ebdc82cb9a2d47a93f6945
Yngve Høiseth <yhoiseth@gmail.com>
2017-01-29 21:14:23 +0100

    Install PHPUnit, add failing step definitions

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -9,6 +10,9 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext implements Context
 {
+    private $shelf;
+    private $basket;
+
     /**
      * Initializes context.
      *
@@ -18,5 +22,45 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+        $this->shelf = new Shelf();
+        $this->basket = new Basket($this->shelf);
+    }
+
+    /**
+     * @Given there is a :product, which costs £:price
+     */
+    public function thereIsAWhichCostsPs($product, $price)
+    {
+        $this->shelf->setProductPrice($product, floatval($price));
+    }
+
+    /**
+     * @When I add the :product to the basket
+     */
+    public function iAddTheToTheBasket($product)
+    {
+        $this->basket->addProduct($product);
+    }
+
+    /**
+     * @Then the overall basket price should be £:price
+     */
+    public function theOverallBasketPriceShouldBePs($price)
+    {
+        PHPUnit_Framework_Assert::assertSame(
+            floatval($price),
+            $this->basket->getTotalPrice()
+        );
+    }
+
+    /**
+     * @Then I should have :count product(s) in the basket
+     */
+    public function iShouldHaveProductsInTheBasket($count)
+    {
+        PHPUnit_Framework_Assert::assertCount(
+            intval($count),
+            $this->basket
+        );
     }
 }
